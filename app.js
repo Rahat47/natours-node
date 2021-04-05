c9ddfaa47851f774ec7510878cabc5e827eaf0f9
Rahat <djrayhan8@gmail.com>
2021-04-05 15:23:16 +0600

    starting with the tours API

--- app.js
+++ app.js
@@ -1,16 +1,67 @@
+import fs from 'fs'
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
-
 const app = express()
 
+//?Dirname problem solved
+const __dirname = path.resolve();
+
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-
-app.get("/", (req, res) => {
-    res.send("Hello from natours API")
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id
+    const tour = tours.find(el => el.id === Number(id))
+
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: "failed",
+            message: "Invalid Id"
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        }
+    })
+})
+
+
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
+})
 
 const port = 5000
 app.listen(port, () => {
