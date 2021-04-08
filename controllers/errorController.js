0c446025d4924aee407e12c8ba840ba8865cdf47
Rahat <djrayhan8@gmail.com>
2021-04-08 20:45:07 +0600

    Error handling fundamentals completed

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,25 @@
 import { nodeEnv } from "../variables.js"
+import AppError from '../utils/appError.js'
+
+//!MongoDB ERROR HANDLERS
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateValueDB = (err) => {
+    const message = `There is another document with the Name "${err.keyValue.name}". The Name must be unique,`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map(er => er.message)
+
+    const message = `Invalid Input Data. ${errors.join(". ")}`
+    return new AppError(message, 400)
+}
+//!^^^^^^^^^^^^ MongoDB Error Handlers End ^^^^^^^^^^^^^//
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -25,7 +46,6 @@ const sendErrorProd = (err, res) => {
             message: "Something went wrong."
         })
     }
-
 }
 
 export const globalErrorHandler = (err, req, res, next) => {
@@ -34,7 +54,20 @@ export const globalErrorHandler = (err, req, res, next) => {
 
     if (nodeEnv === 'development') {
         sendErrorDev(err, res)
+
     } else if (nodeEnv === 'production') {
-        sendErrorProd(err, res)
+        let error = { ...err }
+        console.log(error);
+        if (error.name === "CastError" || error.kind === "ObjectId") {
+            error = handleCastErrorDB(error)
+        }
+        if (error.code === 11000) {
+            error = handleDuplicateValueDB(error)
+        }
+        if (error.name === "ValidationError" || error._message === "Validation failed") {
+            error = handleValidationErrorDB(error)
+        }
+
+        sendErrorProd(error, res)
     }
 }
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+
 import app from "./app.js";
 import mongoose from 'mongoose'
 import chalk from 'chalk'
 import { port, dbUser, dbPass, dbName } from "./variables.js";
 
 const dburi = `mongodb+srv://${dbUser}:${dbPass}@azurehk.jxbko.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+//?LAST STAND FOR ERRORS
+process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXEPTION 💥💥 app is shutting down....");
+    console.log(err.name, err.message);
+    process.exit(1)
+})
 
 
 mongoose.connect(dburi, {
@@ -12,10 +20,19 @@ mongoose.connect(dburi, {
     useCreateIndex: true
 }).then(() => {
     console.log(chalk.bold.yellowBright("database connected and running"));
-    //!LISTEN TO THE APP ONLY AFTED DB IS CONNECTED...
-    app.listen(port, () => {
-        console.log(chalk.bold.blueBright(`App running on port ${port}`));
-    })
 }).catch(err => {
     console.log(err);
+})
+
+
+const server = app.listen(port, () => {
+    console.log(chalk.bold.blueBright(`App running on port ${port}`));
+})
+
+process.on("unhandledRejection", (err) => {
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION 💥💥 app is shutting down....");
+    server.close(() => {
+        process.exit(1)
+    })
 })
